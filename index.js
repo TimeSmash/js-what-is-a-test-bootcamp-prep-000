aa04f804a79bb4172ebc950ad132e3589a49086e
mcumm64@gmail.com <mcumm64@gmail.com>
2018-07-24 21:03:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
 var name = "Princess Peach"
 var place = "Mushroom Kingdom"
-var message = `${name} is ${height} inches tall`
+var message = `${name} is in ${place}`
 
 module.exports = {name, height, message}
 console.log(message)
